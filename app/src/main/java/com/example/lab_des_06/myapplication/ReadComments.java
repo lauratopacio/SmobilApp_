5a739c77f7066f6ac8b52202a1a0349df5ae4e3a
lauratopacio <lauratopaciovaldez@gmail.com>
2018-05-17 09:11:40 -0600

    Initial commit

---
+++ app/src/main/java/com/example/lab_des_06/myapplication/ReadComments.java
@@ -0,0 +1,16 @@
+package com.example.lab_des_06.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by LAB-DES-06 on 17/05/2018.
+ */
+
+public class ReadComments extends ActionBarActivity{
+    protected void onCreate(Bundle savedInstanceState){
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.read_comments);
+    }
+}
